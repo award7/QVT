944f453158315546aa4fa36cc73dd46b88978e36
Aaron Ward <award7@wisc.edu>
2022-05-20 16:10:36 -0500

    init commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyVipr',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/award7/QVT',
+    license='',
+    author='Aaron Ward',
+    author_email='',
+    description='4D Flow MRI Processing in Python'
+)
